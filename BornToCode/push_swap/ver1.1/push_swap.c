3a14b45141af898eeab6873580f21e30bcac475a
Jaehwan Kim <jaekim@c4r6s8.42seoul.kr>
2022-04-26 21:00:23 +0900

    feat : Add sa(swap a)

--- BornToCode/push_swap/ver1.1/push_swap.c
+++ BornToCode/push_swap/ver1.1/push_swap.c
@@ -1,5 +1,14 @@
 #include "push_swap.h"
 
+void	print_lst(t_node *node)
+{
+	while (node)
+	{
+		printf("[%d]: %d\n", node->index, node->value);
+		node = node->next;
+	}
+}
+
 void	ft_swap_a(t_node **node_a)
 {	
 	int i;
@@ -12,23 +21,7 @@ void	ft_swap_a(t_node **node_a)
 	curr -> next -> next -> prev = curr;
 	curr -> next = curr -> next -> next;
 	curr -> prev -> next = curr;
-	
-	// i = 0;
-	// while (0 != node_a)
-	// {
-	// 	printf("After : node_a, av[%d] = %d, index = %d\n", i, node_a->value, node_a->index);
-	// 	node_a = node_a -> next;
-	// 	i++;
-	// }
-}
-
-void	print_lst(t_node *node)
-{
-	while (node)
-	{
-		printf("[%d]: %d\n", node->index, node->value);
-		node = node->next;
-	}
+	*node_a = curr -> prev;
 }
 
 int	main(int ac, char **av)
@@ -47,35 +40,11 @@ int	main(int ac, char **av)
 	deque_b = ft_deqnew(deque_b);
 	deque_a = ft_deqnew(deque_a);
     ft_make_list(av, deque_a, &node_a);
-	//curr = node_a;
-	// while (0 != node_a)
-	// {
-	// 	printf("Befter : node_a, av[%d] = %d, index = %d\n", i, node_a->value, node_a->index);
-	// 	node_a = node_a -> next;
-	// 	i++;
-	// }
-	// printf("-------------------------------------------------\n");
-	//node_a = curr;
 	ft_make_index(node_a);
-	printf("%p\n", node_a->next->next->next->next->next->next->next);
 	print_lst(node_a);
-	// while (0 != node_a)
-	// {
-	// 	printf("Befter : node_a, av[%d] = %d, index = %d\n", i, node_a->value, node_a->index);
-	// 	node_a = node_a -> next;
-	// 	i++;
-	// }
-	// printf("-------------------------------------------------\n");
-	// node_a = curr;
-	// ft_swap_a(&node_a);
-	// printf("After : node_a, av[%d] = %d, index = %d\n", i, node_a->prev->value, node_a->prev->index);
-	// while (0 != node_a)
-	// {
-	// 	printf("After : node_a, av[%d] = %d, index = %d\n", i, node_a->value, node_a->index);
-	// 	node_a = node_a -> next;
-	// 	i++;
-	// }
-	//printf("deque -> head size = %d\n", deque->size);
+	printf("--------\n");
+	ft_swap_a(&node_a);
+	print_lst(node_a);
     //print_stack(node_a);
     //push_swap(first_stack);
 	// system("leaks a.out");
