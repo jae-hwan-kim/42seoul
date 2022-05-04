7413eaa0558fc020852da9c9032a315f51f130c2
김재환 <woghks7859@gmail.com>
2022-05-05 00:09:08 +0900

    feat : creating split...

---
+++ BornToCode/push_swap/ver1.1/testFolder/ft_split.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+int	count_size(char **av)
+{
+	int	i;
+	int	j;
+	int	length;
+
+	i = 1;
+	length = 0;
+	while (0 != av[i])
+	{
+		j = 0;
+		while (0 != av[i][j])
+		{
+			if ((9 <= av[i][j] && 13 >= av[i][j]) || 32 == av[i][j])
+				j++;
+			else
+			{
+				while (0 != av[i][j]
+				 && (9 > av[i][j] || 13 < av[i][j]) && 32 != av[i][j])
+					j++;
+				length++;
+			}
+		}
+		i++;
+	}
+	return (length);
+}
+
+// char	*ft_w_make(char *word, char const *s, int n, int w_len)
+// {
+// 	int		i;
+
+// 	i = 0;
+// 	while (w_len > 0)
+// 	{
+// 		word[i] = s[n - w_len];
+// 		i++;
+// 		w_len--;
+// 	}
+// 	word[i] = '\0';
+// 	return (word);
+// }
+
+// char	**ft_split2(char **result, char const *s, int w_len)
+// {
+// 	int		i;
+// 	int		n;
+// 	int		w_len2;
+
+// 	i = 0;
+// 	n = 0;
+// 	w_len2 = 0;
+// 	while (s[n] && i < w_len)
+// 	{
+// 		while (s[n] && s[n] == c)
+// 			n++;
+// 		while (s[n] && s[n] != c)
+// 		{
+// 			n++;
+// 			w_len2++;
+// 		}
+// 		result[i] = (char *)malloc(sizeof(char) * (w_len2 + 1));
+// 		if (result == 0)
+// 			return (0);
+// 		ft_w_make(result[i], s, n, w_len2);
+// 		w_len2 = 0;
+// 		i++;
+// 	}
+// 	result[i] = 0;
+// 	return (result);
+// }
+
+char	**ft_split(char **av)
+{
+	int		word_len;
+	char	**result;
+
+	result = 0;
+	word_len = count_size(av);
+	// result = (char **)malloc(sizeof(char *) * (word_len + 1));
+	// if (result == 0)
+	// 	return (0);
+	// ft_split2(result, av, word_len);
+	printf("%d\n", word_len);
+	return (result);
+}
+
+int	main(int ac, char **av)
+{
+	ft_split(av);
+	return (0);	
+}
