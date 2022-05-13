d115c8a02bf286915b979de12c0bc835486f4706
김재환 <woghks7859@gmail.com>
2022-05-13 13:18:25 +0900

    feat : Add Checking sort function & sorting small size

--- BornToCode/push_swap/ver1.2/push_swap/push_swap.c
+++ BornToCode/push_swap/ver1.2/push_swap/push_swap.c
@@ -79,9 +79,80 @@ void	b_to_a(t_deque *deque_a, t_deque *deque_b)
 	}
 }
 
+int	check_sorting(t_deque *deque_a)
+{
+	t_node	*node;
+	int	check_index;
+
+	node = deque_a -> head;
+	check_index = deque_a->head->index;
+	while (0 != node)
+	{
+		if (check_index != node -> index)
+			return (0);
+		node = node -> next;	
+		check_index++;
+	}
+	return(1);
+}
+
+void	sort_small_size(t_deque *deque_a, t_deque *deque_b, int size)
+{
+	int	position;
+	t_node	*node;
+
+	node = deque_a->head;
+	position = 0;
+	if (size == 2)
+		ft_rotate_a(deque_a);
+	else if (size == 3)
+	{
+		while (2 != deque_a->tail->index)
+			ft_rotate_a(deque_a);
+		if (check_sorting(deque_a))
+			return ;
+		else
+			ft_swap_a(deque_a);
+	}
+	else if (size == 5)
+	{
+		while (node -> index)
+		{
+			position++;
+			node = node -> next;
+		}
+		if (2 < position)
+			while (deque_a->head->index)
+				ft_reverse_rotate_a(deque_a);
+		else if (2 >= position)
+			while (deque_a->head->index)
+				ft_rotate_a(deque_a);		
+		ft_push_b(deque_a, deque_b);
+		while (1 != deque_a->head->index)
+			ft_rotate_a(deque_a);
+		ft_push_b(deque_a, deque_b);
+		while (4 != deque_a->tail->index)
+			ft_rotate_a(deque_a);
+		if (!check_sorting(deque_a))
+			ft_swap_a(deque_a);
+		ft_push_a(deque_a, deque_b);
+		ft_push_a(deque_a, deque_b);
+	}
+}
+
 void	push_swap(t_deque *deque_a, t_deque *deque_b)
 {
 	//인자가 1개일 때, 2, 3, 4, 5 일 때
-	a_to_b(deque_a, deque_b);
-	b_to_a(deque_a, deque_b);
+	int	size;
+
+	size = deque_a ->size;
+	if (check_sorting(deque_a))
+		return ;
+	else if (size <= 3 || size == 5)
+		sort_small_size(deque_a, deque_b, size);
+	else
+	{
+		a_to_b(deque_a, deque_b);
+		b_to_a(deque_a, deque_b);
+	}
 }
